fe9f532d3b204ac2d739df92857a019b13a6ac7b
CelebradoJonathan <jonathancelebrado@gmail.com>
2019-10-04 17:30:04 +0800

    removed import glob

---
+++ listdir.py
@@ -0,0 +1,36 @@
+import os
+import csv
+import argparse
+
+
+def directory_to_csv(desired_path, desired_filename):
+    """Accepts a desired path and a desired filename,
+    then use the directory to recursively collect data then print in a csv file.
+    Args:
+        desired_path : the directory to be processed
+        desired_filename : the filename of the csv to be created
+    """
+    if os.path.exists(desired_path):
+        with open(desired_filename, 'w+', newline='') as csvfile:
+            fieldnames_csv = ['parent path', 'filename', 'filesize']
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames_csv)
+            writer.writeheader()
+
+            for root, directories, files in os.walk(desired_path):
+
+                for file in files:
+                    file_dict = {"parent path": os.path.dirname(root), "filename": file,
+                                 "filesize": os.path.getsize("{}\{}".format(root, file))}
+                    writer.writerow(file_dict)
+
+    else:
+        print("Path not found")
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("directory")
+    parser.add_argument("csv_name")
+    args = parser.parse_args()
+
+    directory_to_csv(args.directory, args.csv_name)
