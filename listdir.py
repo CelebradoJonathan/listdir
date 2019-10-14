fb72c7f1c780297931b210e0efa20cb6e58431af
CelebradoJonathan <jonathancelebrado@gmail.com>
2019-10-14 15:51:29 +0800

    added zip function in listdir with hash

--- listdir.py
+++ listdir.py
@@ -1,6 +1,19 @@
 import os
 import csv
 import argparse
+import hashlib
+import zipfile
+
+
+def zip_output(filename):
+    """Accepts a desired filename,
+        then zips that file.
+        Args:
+            filename : the filename of the zip file to be created
+    """
+    zip_name = filename + ".zip"
+    with zipfile.ZipFile(zip_name, 'w') as out_zip:
+        out_zip.write(filename)
 
 
 def directory_to_csv(desired_path, desired_filename):
@@ -11,17 +24,31 @@ def directory_to_csv(desired_path, desired_filename):
         desired_filename : the filename of the csv to be created
     """
     if os.path.exists(desired_path):
+        md5_hasher = hashlib.md5()
+        sha1_hasher = hashlib.sha1()
+
         with open(desired_filename, 'w+', newline='') as csvfile:
-            fieldnames_csv = ['parent path', 'filename', 'filesize']
+            fieldnames_csv = ['parent path', 'filename', 'filesize', 'md5', 'sha1']
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames_csv)
             writer.writeheader()
 
             for root, directories, files in os.walk(desired_path):
 
                 for file in files:
-                    file_dict = {"parent path": os.path.dirname(root), "filename": file,
-                                 "filesize": os.path.getsize("{}\{}".format(root, file))}
-                    writer.writerow(file_dict)
+                    filepath = "{}\{}".format(root, file)
+
+                    with open(filepath, 'rb') as afile:
+                        buf = afile.read()
+                        md5_hasher.update(buf)
+                        sha1_hasher.update(buf)
+
+                        file_dict = {"parent path": os.path.dirname(root), "filename": file,
+                                     "filesize": os.path.getsize(filepath), "md5": md5_hasher.hexdigest(),
+                                     "sha1": sha1_hasher.hexdigest()}
+
+                        writer.writerow(file_dict)
+
+        zip_output(desired_filename)
 
     else:
         print("Path not found")
@@ -34,5 +61,3 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     directory_to_csv(args.directory, args.csv_name)
-
-
