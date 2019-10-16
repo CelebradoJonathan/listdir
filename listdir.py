033d01288430021da28c58f5e1171bf045b3bdbb
CelebradoJonathan <jonathancelebrado@gmail.com>
2019-10-16 11:04:34 +0800

    added datetime in filename

--- listdir.py
+++ listdir.py
@@ -4,17 +4,50 @@ import argparse
 import hashlib
 import zipfile
 import configparser
+import datetime
 
 
 def zip_output(filename):
     """Accepts a desired filename,
-        then zips that file.
+    then zips that file.
         Args:
             filename : the filename of the zip file to be created
     """
     zip_name = filename + ".zip"
     with zipfile.ZipFile(zip_name, 'w') as out_zip:
         out_zip.write(filename)
+
+
+def get_hash(file_path, hash_type):
+    """Accepts a desired file_path,
+    then hashes that file depending on the hash_mode.
+        Args:
+            file_path : the file to be hashed.
+            hash_type : the hash type.
+    """
+    with open(file_path, 'rb') as afile:
+        buf = afile.read()
+
+        if hash_type == "sha1":
+            sha1_hasher = hashlib.sha1()
+            sha1_hasher.update(buf)
+            return sha1_hasher.hexdigest()
+        elif hash_type == "md5":
+            md5_hasher = hashlib.md5()
+            md5_hasher.update(buf)
+            return md5_hasher.hexdigest()
+
+
+def add_datetime(file_name, time_format):
+    """Accepts a desired file_name,
+    then formats the filename to include the current date and time.
+        Args:
+            file_name : the file to be formatted.
+            time_format : the format of date time to be used.
+    """
+    curr_date = datetime.datetime.now()
+    str_date = curr_date.strftime(time_format)
+    return str_date + "_" + file_name
 
 
 def directory_to_csv(desired_path, desired_filename):
@@ -25,9 +58,7 @@ def directory_to_csv(desired_path, desired_filename):
         desired_filename : the filename of the csv to be created
     """
     if os.path.exists(desired_path):
-        md5_hasher = hashlib.md5()
-        sha1_hasher = hashlib.sha1()
-
+        desired_filename = add_datetime(desired_filename, '%Y%m%d_%H-%M-%S')
         with open(desired_filename, 'w+', newline='') as csvfile:
             fieldnames_csv = ['parent path', 'filename', 'filesize', 'md5', 'sha1']
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames_csv)
@@ -36,18 +67,13 @@ def directory_to_csv(desired_path, desired_filename):
             for root, directories, files in os.walk(desired_path):
 
                 for file in files:
-                    filepath = "{}\{}".format(root, file)
+                    file_path = "{}\{}".format(root, file)
 
-                    with open(filepath, 'rb') as afile:
-                        buf = afile.read()
-                        md5_hasher.update(buf)
-                        sha1_hasher.update(buf)
+                    file_dict = {"parent path": os.path.dirname(root), "filename": file,
+                                 "filesize": os.path.getsize(file_path), "md5": get_hash(file_path, "md5"),
+                                 "sha1": get_hash(file_path, "sha1")}
 
-                        file_dict = {"parent path": os.path.dirname(root), "filename": file,
-                                     "filesize": os.path.getsize(filepath), "md5": md5_hasher.hexdigest(),
-                                     "sha1": sha1_hasher.hexdigest()}
-
-                        writer.writerow(file_dict)
+                    writer.writerow(file_dict)
 
         zip_output(desired_filename)
 
@@ -59,6 +85,8 @@ if __name__ == '__main__':
 
     config = configparser.ConfigParser()
     config.read("conf.ini")
+    directory = config['args']['directory']
+    filename = config['args']['filename']
 
     parser = argparse.ArgumentParser()
     parser.add_argument("directory", nargs='?')
@@ -68,6 +96,6 @@ if __name__ == '__main__':
     if args.directory and args.csv_name:
         directory_to_csv(args.directory, args.csv_name)
     else:
-        directory_to_csv(config['args']['directory'], config['args']['filename'])
+        directory_to_csv(directory, filename)
 
     # print(config.sections())
